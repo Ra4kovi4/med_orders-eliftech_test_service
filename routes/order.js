9511e768b9371883c8f0f6ae944549f16ae599e3
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-05-25 21:31:59 +0300

    remove authentificate from addOrder

--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { order: controllers } = require("../controllers");
 const { authenticate, validateBody } = require("../middlewars");
-router.post("/", authenticate, controllers.addOrder);
+router.post("/", controllers.addOrder);
 router.get("/", controllers.getAllUserOrders);
 
 module.exports = router;
